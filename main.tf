e49e63b4f30faed14ed04913b9e041570754722c
najouazeryouh <zeryouhnajoua@outlook.fr>
2023-10-05 09:42:36 +0000

    last

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ resource "google_project_service" "api-serviceusage" {
   service = "serviceusage.googleapis.com" 
 }
 
+
 resource "google_project_service" "api-artifact-registry" {
   project = "devops-tp3"
   service = "artifactregistry.googleapis.com" 
@@ -47,4 +48,55 @@ resource "google_sql_user" "wordpress" {
 resource "google_sql_database" "sql-database" {
   name     = "wordpress"
   instance = "main-instance"
+}
+
+resource "google_project_service" "api-cloudrun" {
+  project = "devops-tp3"
+  service = "run.googleapis.com" 
+}
+
+
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-central1"
+
+template {
+   spec {
+      containers {
+      image = "us-central1-docker.pkg.dev/devops-tp3/website-tools/wordpressimg2"
+      ports {
+        container_port = 80
+      }
+      }      
+   }
+
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "devops-tp3:us-central1:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+
+
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
 }
